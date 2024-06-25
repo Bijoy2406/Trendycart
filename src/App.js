959772a15c4a22d333b6ad437b63bf510431f328
orik01556340144 <121636853+orik01556340144@users.noreply.github.com>
2024-06-26 03:52:50 +0600

    shop-update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
@@ -7,44 +8,46 @@ import Product from './pages/Product';
 import Cart from './pages/Cart';
 import Login from './pages/login'; 
 import Footer from './components/Footer/Footer';
+import men_banner from './components/Assets/banner_mens.png';
+import women_banner from './components/Assets/banner_women.png';
+import kids_banner from './components/Assets/banner_kids.png';
+import ShopContextProvider from './components/Context/ShopContext'; // Import ShopContextProvider
 
 function App() {
     return (
         <div>
-            <BrowserRouter>
-                <Routes>
-                    {/* Define a route for Login with no Navbar */}
-                    <Route path='/login/*' element={<Login />} />
-                    
-                    {/* Define other routes with Navbar */}
-                    <Route path='/' element={<WithNavbar />}>
-                        <Route index element={<Shop />} />
-                        <Route path='mens' element={<ShopCategory category='mens' />} />
-                        <Route path='womens' element={<ShopCategory category='womens' />} />
-                        <Route path='kids' element={<ShopCategory category='kids' />} />
-                        <Route path='product' element={<Product />} />
-                        <Route path='cart' element={<Cart />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
+            <ShopContextProvider> {/* Wrap the entire app with ShopContextProvider */}
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/login/*' element={<Login />} />
+                        <Route path='/' element={<WithNavbar />}>
+                            <Route index element={<Shop />} />
+                            <Route path='mens' element={<ShopCategory banner={men_banner} category='men' />} />
+                            <Route path='womens' element={<ShopCategory banner={women_banner} category='women' />} />
+                            <Route path='kids' element={<ShopCategory banner={kids_banner} category='kid' />} />
+                            <Route path='product' element={<Product />} />
+                            <Route path='cart' element={<Cart />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </ShopContextProvider>
         </div>
     );
 }
 
-// Component to wrap routes that should have Navbar
 function WithNavbar() {
     return (
         <div>
             <Navbar />
             <Routes>
                 <Route path='/' element={<Shop />} />
-                <Route path='mens' element={<ShopCategory category='mens' />} />
-                <Route path='womens' element={<ShopCategory category='womens' />} />
-                <Route path='kids' element={<ShopCategory category='kids' />} />
+                <Route path='mens' element={<ShopCategory banner={men_banner} category='men' />} />
+                <Route path='womens' element={<ShopCategory banner={women_banner} category='women' />} />
+                <Route path='kids' element={<ShopCategory banner={kids_banner} category='kid' />} />
                 <Route path='product' element={<Product />} />
                 <Route path='cart' element={<Cart />} />
             </Routes>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
