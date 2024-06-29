a6474b4cb2967ffafed41740a6e92b1257f4190b
orik01556340144 <121636853+orik01556340144@users.noreply.github.com>
2024-06-30 01:15:07 +0600

    Add feature XYZ

---
+++ src/pages/search.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useMemo, useRef, useEffect } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import all_products from '../components/Assets/all_product'; 
+import './CSS/search.css';
+
+const SearchResults = () => {
+  const { searchTerm } = useParams();
+  const [items, setItems] = useState([]);
+  const [query, setQuery] = useState(searchTerm || '');
+  const [minPrice, setMinPrice] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+  const inputRef = useRef();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setQuery(searchTerm || '');
+  }, [searchTerm]);
+
+  const searchResults = useMemo(() => {
+    let filteredProducts = all_products.filter(product =>
+      product.name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    if (minPrice !== '') {
+      filteredProducts = filteredProducts.filter(product => product.new_price >= minPrice);
+    }
+    if (maxPrice !== '') {
+      filteredProducts = filteredProducts.filter(product => product.new_price <= maxPrice);
+    }
+
+    return filteredProducts;
+  }, [query, minPrice, maxPrice]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const value = inputRef.current.value.trim();
+    if (value === '') return;
+    setQuery(value);
+    navigate(`/search/${value}`); 
+    inputRef.current.value = '';
+  };
+
+  return (
+    <div className="search-container">
+     
+      <div className="price-filter">
+        <label>Min Price:</label>
+        <input
+          type="number"
+          value={minPrice}
+          onChange={(e) => setMinPrice(e.target.value)}
+          placeholder="Min Price"
+        />
+        <label>Max Price:</label>
+        <input
+          type="number"
+          value={maxPrice}
+          onChange={(e) => setMaxPrice(e.target.value)}
+          placeholder="Max Price"
+        />
+      </div>
+      <div className="search-results">
+        {searchResults.length > 0 ? (
+          <div className="products-grid">
+            {searchResults.map(product => (
+              <Link to={`/product/${product.id}`} key={product.id} className="product-card">
+                <img className="product-image" src={product.image} alt={product.name} />
+                <h2 className="product-name">{product.name}</h2>
+                <p className="product-price">Price: {product.new_price}</p>
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <div className='not-found'> 
+            <h2>We're sorry.</h2>
+            <p>We cannot find any matches for your search term.</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default SearchResults;
