e535f5232d49ddf4e8d88de8de85be5119ca0f73
orik01556340144 <121636853+orik01556340144@users.noreply.github.com>
2024-06-30 00:07:00 +0600

    bug-fix

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
+import Shop from './pages/Shop';
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/login';
+import Footer from './components/Footer/Footer';
+import men_banner from './components/Assets/banner_mens.png';
+import women_banner from './components/Assets/banner_women.png';
+import kids_banner from './components/Assets/banner_kids.png';
+import ShopContextProvider from './components/Context/ShopContext'; // Import ShopContextProvider
+import SearchResults from './pages/search'; // Import SearchResults
+
+function App() {
+  return (
+    <div>
+      <ShopContextProvider> {/* Wrap the entire app with ShopContextProvider */}
+        <BrowserRouter>
+          <Routes>
+            <Route path='/login/*' element={<Login />} />
+            <Route path='*' element={<WithNavbar />}>
+              <Route index element={<Shop />} />
+              <Route path='mens' element={<ShopCategory banner={men_banner} category='men' />} />
+              <Route path='womens' element={<ShopCategory banner={women_banner} category='women' />} />
+              <Route path='kids' element={<ShopCategory banner={kids_banner} category='kid' />} />
+              <Route path='product/:productId' element={<Product />} />
+              <Route path='cart' element={<Cart />} />
+              <Route path='search/:searchTerm' element={<SearchResults />} /> {/* Add this line */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+function WithNavbar() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop />} />
+        <Route path='mens' element={<ShopCategory banner={men_banner} category='men' />} />
+        <Route path='womens' element={<ShopCategory banner={women_banner} category='women' />} />
+        <Route path='kids' element={<ShopCategory banner={kids_banner} category='kid' />} />
+        <Route path='product/:productId' element={<Product />} />
+        <Route path='cart' element={<Cart />} />
+        <Route path='search/:searchTerm' element={<SearchResults />} /> {/* Add this line */}
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
