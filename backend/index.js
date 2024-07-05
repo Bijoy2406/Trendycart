3d86c3e6cc20eb859191e144c131188574792a11
orik01556340144 <121636853+orik01556340144@users.noreply.github.com>
2024-07-06 01:24:11 +0600

    fix

---
+++ backend/index.js
@@ -0,0 +1,137 @@
+const port = 4001;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://labibfarhan285:CR7@cluster0.m7lnrxb.mongodb.net/TRANDYCART");
+
+app.get("/", (req, res) => {
+    res.send("Express App is running");
+});
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({ storage: storage });
+app.use('/images', express.static('upload/images'));
+
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    });
+});
+
+const Product = mongoose.model("Product", {
+    id: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    new_price: {
+        type: Number,
+        required: true,
+    },
+    old_price: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    available: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+app.post('/addproduct', async (req, res) => {
+    try {
+       
+        let products = await Product.find({});
+        let id;
+
+       
+        if (products.length > 0) {
+            let last_product_array = products.slice(-1);
+            let last_product =last_product_array[0];
+            id = last_product.id + 1;
+        } else {
+            id = 1;
+        }
+
+      
+        const newProduct = new Product({
+            id: id,
+            name: req.body.name,
+            image: req.body.image,
+            category: req.body.category,
+            new_price: req.body.new_price,
+            old_price: req.body.old_price,
+        });
+
+       
+        await newProduct.save();
+
+        console.log("Saved");
+        res.json({
+            success: true,
+            name: req.body.name,
+        });
+    } catch (error) {
+        console.error("Error saving product:", error);
+        res.status(500).json({
+            success: false,
+            message: "Error saving product",
+        });
+    }
+});
+
+app.post('/remove',async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+app.get('/getall',async(req,res)=>{
+   
+    let products = await Product.find({});
+    console.log("All product fetched.");
+    res.send(products);
+
+
+})
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server Running on Port " + port);
+    } else {
+        console.log("Error: " + error);
+    }
+});
