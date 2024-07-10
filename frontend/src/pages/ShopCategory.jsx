4da16107deb88e042c542e160af2a4a0ec0b803a
bijoy2406 <you@example.com>
2024-07-10 18:22:58 +0600

    update

--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -1,18 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './CSS/ShopCategory.css';
 import { ShopContext } from '../components/Context/ShopContext';
-import dropdown_icon from '../components/Assets/dropdown_icon.png'
-import Item from '../components/Item/Item'
+import dropdown_icon from '../components/Assets/dropdown_icon.png';
+import Item from '../components/Item/Item';
 
 const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  // Update the filteredProducts state whenever all_product changes
+  useEffect(() => {
+    const updatedProducts = all_product.filter(item => item.category === props.category);
+    setFilteredProducts(updatedProducts);
+  }, [all_product, props.category]);
 
   return (
     <div className='shop-category'>
       <img className='shopcategory-banner' src={props.banner} alt="Category Banner" />
       <div className="shopcategory-indexsort">
         <p>
-          <span>Showing 1-12 </span> out of 36 products
+          <span>Showing 1-{filteredProducts.length} </span> out of {filteredProducts.length} products
         </p>
 
         <div className="shopcategory-sort">
@@ -20,24 +27,21 @@ const ShopCategory = (props) => {
         </div>
       </div>
       <div className="shopcategory-products">
-        {
-          all_product.map((item, i) => {
-            if (props.category === item.category) {
-              return <Item key={i}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                new_price={item.new_price}
-                old_price={item.old_price}
-              />
-            } else {
-              return null;
-            }
-          })}
+        {filteredProducts.map((item, index) => (
+          <Item key={index}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
-      <div className="shopcategory-loadmore">
-        Explore More
-      </div>
+      {filteredProducts.length > 0 && (
+        <div className="shopcategory-loadmore">
+          Explore More
+        </div>
+      )}
     </div>
   );
 };
