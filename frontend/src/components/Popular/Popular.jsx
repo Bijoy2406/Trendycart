18b092d5a73adac59dbc490cdc0da7ce65f644f7
orik01556340144 <121636853+orik01556340144@users.noreply.github.com>
2024-07-17 04:11:24 +0600

    first

---
+++ frontend/src/components/Popular/Popular.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'; 
+import './Popular.css';
+import Item from '../Item/Item';
+
+const Popular = () => {
+  const [data_product, setPopularProducts] = useState([]); 
+
+  useEffect(() => {
+    fetch('http://localhost:4001/polpularinwoman')
+      .then((response) => response.json())
+      .then((data) => setPopularProducts(data));
+     
+  }, []); 
+
+  return (
+    <div className='popular'>
+       <h1>POPULAR IN WOMEN</h1> 
+       <hr />
+       <div className="popular-item">
+        {data_product.map((item, i) => (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+        ))}
+       </div>
+    </div>
+  );
+};
+
+export default Popular;
