bef19b32bf93a03aaa36e91d0dad0a44776003b1
orik01556340144 <121636853+orik01556340144@users.noreply.github.com>
2024-07-18 02:07:46 +0600

    fix

--- backend/index.js
+++ backend/index.js
@@ -221,17 +221,19 @@ app.get('/polpularinwoman', async (req, res) => {
 const fetchUser = async (req, res, next) => {
     const token = req.header('auth-token');
     if (!token) {
-        res.status(401).send({ error: "Not any Token " });
+        res.status(401).send({ error: "No Token Provided" });
     } else {
         try {
             const data = jwt.verify(token, 'secret_ecom');
             req.user = data.user;
             next();
         } catch (error) {
-            res.status(401).send({ errors: "please give correct token" });
+            res.status(401).send({ errors: "Invalid Token" });
         }
     }
 };
+
+
 
 app.post('/addtocart', fetchUser, async (req, res) => {
     console.log("Addtocart", req.body.itemId);
