5859f92d7937bbe92e598cd6e0db618e16fee354
Bijoy <bijoy.ahmed12555@gmail.com>
2024-09-21 23:40:16 +0600

    update

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = () => {
   return (
     <div className='hero'>
       <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
-        <div>
+      <span className="hero-title">NEW ARRIVALS ONLY</span>
+      <div>
           <div className="hero-hand-icon">
             <p>new</p>
             <img src={hand_icon} alt="" />
